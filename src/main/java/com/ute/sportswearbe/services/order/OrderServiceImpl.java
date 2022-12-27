9d18a93c881dbae378d727a0a06c7a6d6033e1a2
NVD-NVD <datnguyen2847@gmail.com>
2022-12-27 23:03:35 +0700

    Fix sub quantity when create order

--- src/main/java/com/ute/sportswearbe/services/order/OrderServiceImpl.java
+++ src/main/java/com/ute/sportswearbe/services/order/OrderServiceImpl.java
@@ -4,8 +4,10 @@ import com.ute.sportswearbe.dtos.OrderDto;
 import com.ute.sportswearbe.entities.Order;
 import com.ute.sportswearbe.entities.Product;
 import com.ute.sportswearbe.entities.User;
+import com.ute.sportswearbe.entities.embedded.EmbeddedOption;
 import com.ute.sportswearbe.entities.embedded.EmbeddedPrice;
 import com.ute.sportswearbe.entities.embedded.EmbeddedProductsInOrder;
+import com.ute.sportswearbe.entities.embedded.EmbeddedSize;
 import com.ute.sportswearbe.exceptions.InvalidException;
 import com.ute.sportswearbe.exceptions.NotFoundException;
 import com.ute.sportswearbe.repositories.OrderRepository;
@@ -22,6 +24,7 @@ import org.springframework.stereotype.Service;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
@@ -90,13 +93,51 @@ public class OrderServiceImpl implements OrderService{
                     EmbeddedProductsInOrder embeddedProductsInOrder = new EmbeddedProductsInOrder();
                     embeddedProductsInOrder.setProduct(product);
                     embeddedProductsInOrder.setQuantity(e.getQuantity());
+                    embeddedProductsInOrder.setOptions(e.getOptions());
+                    embeddedProductsInOrder.setDiscount(product.getDiscount());
                     float price = product.getPrice().getPrice();
                     float total = (price - price*(product.getDiscount()/100)) * e.getQuantity();
                     embeddedProductsInOrder.setTotal(total);
                     productsInOrder.add(embeddedProductsInOrder);
                 });
-
         order.setProductsInOrder(productsInOrder);
+
+        List<Product> products = new ArrayList<>();
+        for (int i = 0; i < productsInOrder.size(); i++) {
+            EmbeddedProductsInOrder emp = productsInOrder.get(i);
+            Product product = emp.getProduct();
+            if (!product.isEnable()){
+                throw new InvalidException(String.format("Product có id %s đã bị khoá.", product.getId()));
+            }
+
+            List<EmbeddedSize> emSize = emp.getOptions().getSizes();
+            List<EmbeddedSize> proSize = product.getOptions().getSizes();
+            for (int j = 0; j < emSize.size(); j++) {
+                String emSizeTitle = emSize.get(j).getSize().toLowerCase();
+                int emSizeQuantity = emSize.get(j).getQuantity();
+
+                for (int k = 0; k < proSize.size(); k++) {
+                    EmbeddedSize size = proSize.get(k);
+                    String proSizeTitle = size.getSize().toLowerCase();
+                    int proSizeQuantity = size.getQuantity();
+                    if (emSizeTitle.toLowerCase().equals(proSizeTitle.toLowerCase())){
+                        if (emSizeQuantity <= proSizeQuantity){
+                            proSizeQuantity = proSizeQuantity - emSizeQuantity;
+                            size.setQuantity(proSizeQuantity);
+                            proSize.set(k, size);
+                            product.setQuantity(product.getQuantity()-emSizeQuantity);
+                        }else {
+                            throw new InvalidException(String.format("Product có id %s không đủ số lượng cho Size %s.", product.getId(), emSizeTitle.toUpperCase()));
+                        }
+                    }
+                }
+            }
+            EmbeddedOption option = product.getOptions();
+            option.setSizes(proSize);
+            product.setOptions(option);
+            products.add(product);
+        }
+
         float total = 0.0F;
         for (EmbeddedProductsInOrder embeddedBookInOrder : productsInOrder) {
             total += embeddedBookInOrder.getTotal();
@@ -113,8 +154,10 @@ public class OrderServiceImpl implements OrderService{
         order.setUpdateOn(new Date());
         order.setPayment(null);
         order.setProcessing(EnumProcessing.Chua_Xu_Ly.name());
-        
+
         order.setEnable(true);
+
+        products.forEach((e) -> productService.save(e));
 
         return orderRepository.save(order);
     }
@@ -150,7 +193,7 @@ public class OrderServiceImpl implements OrderService{
 
         order.setProcessing(processing);
 
-        return orderRepository.save(order);
+        return save(order);
     }
 
     @Override
@@ -173,12 +216,4 @@ public class OrderServiceImpl implements OrderService{
         return orderRepository.save(order);
     }
 
-//    private Order convertOrderDto(OrderDto dto){
-//        Order order = new Order();
-//        order.setBooksInOrder(order.getBooksInOrder());
-//        if (dto.getAddressForShipping() != null)
-//            order.setAddress(dto.getAddressForShipping());
-//        order.setNote(dto.getNote());
-//        return order;
-//    }
 }
